28261acbf8ff5eb2110f20166e4e452ffeddd829
atilla.citirik <atillactrk@gmail.com>
2024-05-08 01:49:19 +0300

    Alert, Action Sheet and custom Alert

--- KullaniciEtkilesimi/ViewController.swift
+++ KullaniciEtkilesimi/ViewController.swift
@@ -14,6 +14,62 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAlert(_ sender: Any) {
+        let alertContoller = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .alert)
+        let iptalAction = UIAlertAction(title: "iptal", style: .cancel){ action in
+            print("iptal seçildi")
+        }
+        
+        alertContoller.addAction(iptalAction)
+        
+        let tamamAction = UIAlertAction(title: "Tamam", style: .destructive){ action in
+            print("Tamam seçildi")
+        }
+        
+        alertContoller.addAction(tamamAction)
+        
+        self.present(alertContoller, animated: true)
+    }
+    
+    @IBAction func buttonActionSheet(_ sender: Any) {
+        
+        let alertContoller = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .actionSheet)
+        let iptalAction = UIAlertAction(title: "iptal", style: .cancel){ action in
+            print("iptal seçildi")
+        }
+        
+        alertContoller.addAction(iptalAction)
+        
+        let tamamAction = UIAlertAction(title: "Tamam", style: .destructive){ action in
+            print("Tamam seçildi")
+        }
+        
+        alertContoller.addAction(tamamAction)
+        
+        self.present(alertContoller, animated: true)
+        
+    }
+    @IBAction func buttonOzelAlert(_ sender: Any) {
+        
+        let alertContoller = UIAlertController(title: "Başlık", message: "Mesaj", preferredStyle: .alert)
+        
+        alertContoller.addTextField { textField in
+            textField.placeholder = ",veri Giriniz"
+            textField.keyboardType = .numberPad
+            textField.isSecureTextEntry = true
+            
+        }
+        
+        let kaydetAction = UIAlertAction(title: "Kaydet", style: .cancel){ action in
+            let tf = alertContoller.textFields![0] as UITextField
+            if let alinanVeri = tf.text {
+                print("alınan veri \(alinanVeri)")
+            }
+        }
+        alertContoller.addAction(kaydetAction)
+        
+        self.present(alertContoller, animated: true)
+        
+    }
 }
 
